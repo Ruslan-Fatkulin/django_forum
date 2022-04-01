3eb34bde27b1486dd81ecdc5eed570d79a8d8346
Ruslan-Fatkulin <aka.ruslanjon25@gmail.com>
2022-04-01 19:37:26 +0500

    'commit'

--- community/views.py
+++ community/views.py
@@ -4,4 +4,4 @@ from .models import *
 
 def questions_page(request):
     questions = Question.objects.all()
-    return render(request, 'questions_page.html', {'questions': questions}git remote add origin https://github.com/neruslvn/django4.git)
+    return render(request, 'questions_page.html', {'questions': questions})
